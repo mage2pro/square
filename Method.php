0e6a95598359d1038442ee427c635f780c6352ac
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-01-16 10:56:26 +0300

    no message

--- Method.php
+++ Method.php
@@ -44,6 +44,34 @@ class Method extends \Df\Payment\Method {
 		/** @var SquareTransaction $transaction */
 		$transaction = $response->getTransaction();
 		$this->ii()->setTransactionId($transaction->getId());
+		/**
+		 * 2016-03-15
+		 * Если оставить открытой транзакцию «capture»,
+		 * то операция «void» (отмена авторизации платежа) будет недоступна:
+		 * https://github.com/magento/magento2/blob/2.1.0/app/code/Magento/Sales/Model/Order/Payment.php#L540-L555
+		 * @used-by \Magento\Sales\Model\Order\Payment::canVoid()
+		 * Транзакция считается закрытой, если явно не указать «false».
+		 *
+		 * 2017-01-16
+		 * Наоборот: если закрыть транзакцию типа «authorize»,
+		 * то операция «Capture Online» из административного интерфейса будет недоступна:
+		 * @see \Magento\Sales\Model\Order\Payment::canCapture()
+				if ($authTransaction && $authTransaction->getIsClosed()) {
+					$orderTransaction = $this->transactionRepository->getByTransactionType(
+						Transaction::TYPE_ORDER,
+						$this->getId(),
+						$this->getOrder()->getId()
+					);
+					if (!$orderTransaction) {
+						return false;
+					}
+				}
+		 * https://github.com/magento/magento2/blob/2.1.3/app/code/Magento/Sales/Model/Order/Payment.php#L263-L281
+		 * «How is \Magento\Sales\Model\Order\Payment::canCapture() implemented and used?»
+		 * https://mage2.pro/t/650
+		 * «How does Magento 2 decide whether to show the «Capture Online» dropdown
+		 * on a backend's invoice screen?»: https://mage2.pro/t/2475
+		 */
 		$this->ii()->setIsTransactionClosed($capture);
 		/** @var Tender $tender */
 		$tender = df_first($transaction->getTenders());
