c3f36cac7a3eea31e0920d0289ccd9d529097cd0
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-01-22 13:57:22 +0300

    no message

--- Charge.php
+++ Charge.php
@@ -1,18 +1,14 @@
 <?php
 // 2016-10-06
 namespace Dfe\Square;
-use Dfe\Square\Settings as S;
 class Charge extends \Df\Payment\Charge\WithToken {
 	/**
 	 * 2016-10-06
 	 * https://docs.connect.squareup.com/articles/processing-payment-php/#chargingcardnonce
 	 * @return array(string => mixed)
 	 */
-	private function _request() {/** @var Settings $s */ $s = S::s(); return [
-		'amount_money' => [
-			'amount' => $this->amountF()
-			,'currency' => $this->currencyC()
-		]
+	private function _request() {return [
+		'amount_money' => ['amount' => $this->amountF(), 'currency' => $this->currencyC()]
 		,'card_nonce' => $this->token()
 		,'idempotency_key' => uniqid()
 	];}
@@ -25,11 +21,9 @@ class Charge extends \Df\Payment\Charge\WithToken {
 	 * @param float|null $amount [optional]
 	 * @return array(string => mixed)
 	 */
-	public static function request(Method $method, $token, $amount = null) {return
-		(new self([
-			self::$P__AMOUNT => $amount
-			, self::$P__METHOD => $method
-			, self::$P__TOKEN => $token
-		]))->_request();
-	}
+	public static function request(Method $method, $token, $amount = null) {return (new self([
+		self::$P__AMOUNT => $amount
+		,self::$P__METHOD => $method
+		,self::$P__TOKEN => $token
+	]))->_request();}
 }
