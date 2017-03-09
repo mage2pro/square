6aaf6034464a3639ed34c07dec588d0d6cdd8a4d
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-09 06:07:25 +0300

    no message

--- Block/Info.php
+++ Block/Info.php
@@ -1,12 +1,7 @@
 <?php
 namespace Dfe\Square\Block;
-use Dfe\Square\Method;
-use Magento\Sales\Model\Order\Payment\Transaction as T;
-/**
- * 2016-10-06
- * @final
- * @method Method m()
- */
+// 2016-10-06
+/** @final Unable to use the PHP «final» keyword because of the M2 code generation. */
 class Info extends \Df\Payment\Block\Info {
 	/**
 	 * 2016-10-06
@@ -14,7 +9,7 @@ class Info extends \Df\Payment\Block\Info {
 	 * @see \Df\Payment\Block\Info::prepare()
 	 * @used-by \Df\Payment\Block\Info::_prepareSpecificInformation()
 	 */
-	protected function prepare() {
-		$this->si('Card', df_cc_s($this->ii()->getCcType(), '****' . $this->ii()->getCcLast4()));
-	}
+	final protected function prepare() {$i = $this->ii(); $this->si(
+		'Card', df_cc_s($i->getCcType(), '****' . $i->getCcLast4())
+	);}
 }
