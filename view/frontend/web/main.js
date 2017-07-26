77ab855c1f90314e6a874978ecededef3542d8ac
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-27 00:34:34 +0300

    1.2.2: «Sometimes getting duplicate orders in checkout»: https://mage2.pro/t/4217

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -89,6 +89,7 @@ define([
 							errorsA.push(error.message);
 						});
 						_this.showErrorMessage(errorsA.join("\n"));
+						this.isPlaceOrderActionAllowed(true);
 					}
 				},
 				paymentFormLoaded: function() {
@@ -103,9 +104,7 @@ define([
 						_this.square.setPostalCode(postalCode);
 					}
 					else {
-						$.when(dfc.geo()).then(function(data) {
-							_this.square.setPostalCode(data['zip_code']);
-						});
+						$.when(dfc.geo()).then(function(data) {_this.square.setPostalCode(data['zip_code']);});
 					}
 				}
 			}
@@ -166,9 +165,31 @@ define([
 	/**
 	 * 2016-09-28
 	 * @override
-	 * @see https://github.com/magento/magento2/blob/2.1.0/app/code/Magento/Checkout/view/frontend/web/js/view/payment/default.js#L127-L159
-	 * @used-by https://github.com/magento/magento2/blob/2.1.0/lib/web/knockoutjs/knockout.js#L3863
+	 * @see Df_StripeClone/main::placeOrder()
+	 * @used-by Df_Payment/main.html:
+	 *		<button
+	 *			class="action primary checkout"
+	 *			type="submit"
+	 *			data-bind="
+	 *				click: placeOrder
+	 *				,css: {disabled: !isPlaceOrderActionAllowed()}
+	 *				,enable: dfIsChosen()
+	 *			"
+	 *			disabled
+	 *		>
+	 *			<span data-bind="df_i18n: 'Place Order'"></span>
+	 *		</button>
+	 * https://github.com/mage2pro/core/blob/2.9.10/Payment/view/frontend/web/template/main.html#L57-L68
+	 * https://github.com/magento/magento2/blob/2.1.0/lib/web/knockoutjs/knockout.js#L3863
 	 * @param {this} _this
+	 * @param {Event} event
 	*/
-	placeOrder: function(_this) {this.square.requestCardNonce();}
+	placeOrder: function(_this, event) {
+		if (event) {
+			event.preventDefault();
+		}
+		// 2017-07-26 «Sometimes getting duplicate orders in checkout»: https://mage2.pro/t/4217
+		this.isPlaceOrderActionAllowed(false);
+		this.square.requestCardNonce();
+	}
 });});
