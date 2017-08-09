6a4ac05a56c5155f2196cdc5c880048c71a68285
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-08-09 16:47:04 +0300

    no message

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -1,15 +1,21 @@
 <?php
-// 2016-09-28
 namespace Dfe\Square\Controller\Index;
 use Df\Framework\Controller\Result\Json;
-// 2016-09-28
-// https://github.com/square/connect-api-examples/blob/f83e81/connect-examples/v1/php/webhooks.php
-// https://medium.com/square-corner-blog/webhooks-in-the-square-connect-api-d4d38c4b4d9f
+/**     
+ * 2016-09-28
+ * @final Unable to use the PHP «final» keyword here because of the M2 code generation. 
+ * https://github.com/square/connect-api-examples/blob/f83e81/connect-examples/v1/php/webhooks.php
+ * https://medium.com/square-corner-blog/webhooks-in-the-square-connect-api-d4d38c4b4d9f
+ */
 class Index extends \Magento\Framework\App\Action\Action {
 	/**
-	 * 2016-09-28
+	 * 2016-09-28 
+	 * @final Unable to use the PHP «final» keyword here because of the M2 code generation.
 	 * @override
-	 * @see \Magento\Framework\App\Action\Action::execute()
+	 * @see \Magento\Framework\App\Action\Action::execute()  
+	 * @used-by \Magento\Framework\App\Action\Action::dispatch():
+	 * 		$result = $this->execute();
+	 * https://github.com/magento/magento2/blob/2.2.0-RC1.8/lib/internal/Magento/Framework/App/Action/Action.php#L84-L125
 	 * @return Json
 	 */
 	function execute() {return Json::i('OK');}
