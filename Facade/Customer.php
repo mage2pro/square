1958f6405193845e90e56b37331bfef1a5295d96
Dmitry Fedyuk <dfediuk@gmail.com>
2018-11-13 10:19:04 +0600

    no message

--- Facade/Customer.php
+++ Facade/Customer.php
@@ -101,7 +101,7 @@ final class Customer extends \Df\StripeClone\Facade\Customer {
 	 * @override
 	 * @see \Df\StripeClone\Facade\Customer::_get()
 	 * @used-by \Df\StripeClone\Facade\Customer::get()
-	 * @param int $id
+	 * @param string $id
 	 * @return Operation|null
 	 */
 	protected function _get($id) {return df_try(function() use($id) {return (new C)->get($id);});}
