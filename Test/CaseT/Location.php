e72f7be0e3f23447b103dd9de2068802a8b7c879
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-09 05:32:43 +0000

    https://github.com/mage2pro/core/issues/168

--- Test/CaseT/Location.php
+++ Test/CaseT/Location.php
@@ -4,10 +4,10 @@ use Dfe\Square\API\Facade\Location as L;
 # 2017-10-08
 final class Location extends \Dfe\Square\Test\CaseT {
 	/** 2017-10-08 @test */
-	function t00() {}
+	function t00():void {}
 
 	/** 2017-10-08 [Square] An example of a response to `GET /v2/locations`: https://mage2.pro/t/4647 */
-	function t01_all() {
+	function t01_all():void {
 		try {
 			print_r((new L)->all()->j());
 		}
@@ -20,7 +20,7 @@ final class Location extends \Dfe\Square\Test\CaseT {
 	}
 
 	/** 2017-10-08 */
-	function t02_map() {
+	function t02_map():void {
 		try {
 			print_r((new L)->map());
 		}
