718898f722361d16749db9485ec57846a57e6144
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-09 05:18:40 +0000

    https://github.com/mage2pro/core/issues/168

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -16,7 +16,6 @@ class Index extends \Magento\Framework\App\Action\Action {
 	 * @used-by \Magento\Framework\App\Action\Action::dispatch():
 	 * 		$result = $this->execute();
 	 * https://github.com/magento/magento2/blob/2.2.1/lib/internal/Magento/Framework/App/Action/Action.php#L84-L125
-	 * @return Json
 	 */
-	function execute() {return Json::i('OK');}
+	function execute():Json {return Json::i('OK');}
 }
